1fe09f1cd043b06bf2ddee7fe353cae7d0c53064
Onkar Parmar <onkar@Digishaala.local>
2022-01-26 17:35:45 +0530

    segregate main and test, add test for assertThrows

--- src/test/java/com/digishaala/junit5/calculator/CalculatorTest.java
+++ src/test/java/com/digishaala/junit5/calculator/CalculatorTest.java
@@ -1,15 +1,13 @@
 package com.digishaala.junit5.calculator;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
     @Test
-    @DisplayName("Test Integer Addition")
+    @DisplayName("Test Addition of 2 Integers")
     void shouldTestIntegerAddition() {
 
         // Arrange
@@ -19,6 +17,30 @@ class CalculatorTest {
         int result = calculator.add(1, 2);
 
         // Assert
-        assertEquals(3, result);
+        Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    @DisplayName("Test Division of 2 Integers")
+    void shouldTestIntegerDivision() {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.divide(10, 2);
+
+        // Assert
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    @DisplayName("Test Division by 0 - Should Result in an Exception")
+    void shouldThrowExceptionWhenDivisibleBy0() {
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        // Assert
+        Assertions.assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0), "Division by 0 is not allowed");
     }
 }
